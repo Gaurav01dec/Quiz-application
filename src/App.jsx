6fe4f9f6481a1cf58792e7b6d2bc44af96d7eae7
Gaurav01dec <tech.gauravverma@gmail.com>
2024-05-29 16:27:12 +0530

    beforeAddingTimer

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,16 @@
-function App() {}
+import Header from "./components/header";
+import Quiz from "./components/Quiz";
+
+function App() {
+    return (
+        <>
+            <Header />
+            <main>
+                <Quiz />
+            </main>
+        </>
+
+    )
+}
 
 export default App;
